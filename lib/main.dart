8a533ec292b03d9c4ff37f7337432bd1512346c6
BarkaBoss <bossmajor.biii@gmail.com>
2023-03-06 17:13:17 +0100

    Added Pop Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:daily_planner/screens/intro_screen.dart';
 import 'package:daily_planner/screens/tasks_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter("hive_box");
   runApp(const MyApp());
 }
 
--- lib/screens/tasks_page.dart
+++ lib/screens/tasks_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
@@ -8,12 +9,59 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  double? _deviceHeight, _deviceWidth;
+  String? content;
+  Box? _box;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("To Do!"),
-      )
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight! * 0.1,
+        title: const Text("Daily Planner"),
+      ),
+      body: _tasksWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: displayTaskPop,
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  Widget _tasksWidget(){
+    return FutureBuilder(
+        builder: (BuildContext context, AsyncSnapshot snapshot){
+      if(snapshot.hasData){
+        _box = snapshot.data;
+        return Center();
+      }else{
+        return Center(child: const CircularProgressIndicator());
+      }
+    });
+  }
+
+  void displayTaskPop(){
+    showDialog(context: context, builder: (BuildContext _context){
+      return AlertDialog(
+        title: const Text("Add a ToDo"),
+        content: TextField(
+          onSubmitted: (value){
+
+            setState(() {
+              print(value);
+              Navigator.pop(context);
+            });
+          },
+          onChanged: (value){
+            setState(() {
+              content = value;
+              print(value);
+            });
+          },
+        ),
+      );
+    });
   }
 }
