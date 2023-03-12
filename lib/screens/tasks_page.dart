db14395b744bfdd37a5680394bcae610ef7e8a63
BarkaBoss <bossmajor.biii@gmail.com>
2023-03-12 02:13:51 +0100

    Todo App Done

--- lib/screens/tasks_page.dart
+++ lib/screens/tasks_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import '../models/task.dart';
+
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
 
@@ -30,38 +32,73 @@ class _TasksPageState extends State<TasksPage> {
     );
   }
 
-  Widget _tasksWidget(){
+  Widget _todoList() {
+    List tasks = _box!.values.toList();
+    return ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          var task = Task.fromMap(tasks[index]);
+          return ListTile(
+            title: Text(task.todo),
+            subtitle: Text(task.timeStamp.toString()),
+            trailing: task.done
+                ? Icon(
+                    Icons.check_box_outlined,
+                    color: Colors.green,
+                  )
+                : Icon(Icons.check_box_outline_blank),
+            onTap: () {
+              task.done = !task.done;
+              _box!.putAt(index, task.toMap());
+              setState(() {});
+            },
+            onLongPress: () {
+              _box!.deleteAt(index);
+              setState(() {});
+            },
+          );
+        });
+  }
+
+  Widget _tasksWidget() {
     return FutureBuilder(
-        builder: (BuildContext context, AsyncSnapshot snapshot){
-      if(snapshot.hasData){
+      future: Hive.openBox("tasks"),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+      if (snapshot.hasData) {
         _box = snapshot.data;
-        return Center();
-      }else{
+        return _todoList();
+      } else {
         return Center(child: const CircularProgressIndicator());
       }
     });
   }
 
-  void displayTaskPop(){
-    showDialog(context: context, builder: (BuildContext _context){
-      return AlertDialog(
-        title: const Text("Add a ToDo"),
-        content: TextField(
-          onSubmitted: (value){
-
-            setState(() {
-              print(value);
-              Navigator.pop(context);
-            });
-          },
-          onChanged: (value){
-            setState(() {
-              content = value;
-              print(value);
-            });
-          },
-        ),
-      );
-    });
+  void displayTaskPop() {
+    showDialog(
+        context: context,
+        builder: (BuildContext _context) {
+          return AlertDialog(
+            title: const Text("Add a ToDo"),
+            content: TextField(
+              onSubmitted: (value) {
+                if (content != null) {
+                  var task = Task(
+                      todo: content!, timeStamp: DateTime.now(), done: false);
+                  _box!.add(task.toMap());
+                  setState(() {
+                    print(value);
+                    Navigator.pop(context);
+                  });
+                }
+              },
+              onChanged: (value) {
+                setState(() {
+                  content = value;
+                  print(value);
+                });
+              },
+            ),
+          );
+        });
   }
 }
